ef919f6cd53fcde1352314057ce00acf7ec1124d
Christoph Bessei <chris@bessei-it.eu>
2017-12-19 16:12:07 +0100

    * Fix styling of flash "embed" elements * Bump version to 0.0.3

--- Configuration/TypoScript/Common/Media.ts
+++ Configuration/TypoScript/Common/Media.ts
@@ -1,3 +1,3 @@
-# Style embed element
+# Style embed element, pluginspage attribute is used for Flash elements
 page.cssInline.1513105926 = TEXT
-page.cssInline.1513105926.value = embed {width: 100%;height: 410px;}
+page.cssInline.1513105926.value = embed:not([pluginspage]) {width: 100%;height: 410px;}
