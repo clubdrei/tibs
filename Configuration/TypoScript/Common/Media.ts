310bdee8086e15a7db0c37e93712e536951cb8aa
Christoph Bessei <chris@bessei-it.eu>
2018-03-20 13:17:03 +0100

    Set max-width:100%; on <video> elements

--- Configuration/TypoScript/Common/Media.ts
+++ Configuration/TypoScript/Common/Media.ts
@@ -1,3 +1,7 @@
 # Style embed element, pluginspage attribute is used for Flash elements
 page.cssInline.1513105926 = TEXT
 page.cssInline.1513105926.value = embed:not([pluginspage]) {width: 100%;height: 410px;}
+
+# Set dimensions of video elements
+page.cssInline.1521547707 = TEXT
+page.cssInline.1521547707.value = video {max-width:100%;}
